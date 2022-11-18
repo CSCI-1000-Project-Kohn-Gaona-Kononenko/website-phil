23a733843c957d635d45d501cefa0dc9029f3ecc
PhilKononenko01 <112355240+PhilKononenko01@users.noreply.github.com>
2022-11-18 12:33:01 -0700

    Add files via upload

---
+++ ronBurgundy.js
@@ -0,0 +1,122 @@
+export default function examplePage() {
+    return (
+      <div className='container'>
+        <title>Website</title>
+        <link rel='icon' href='/favicon.ico' />
+
+        <main>
+          <div className='underline'>
+            <h1 className='title'>I'm Phillip Kononenko</h1>
+          </div>
+          <img
+            className='pic'
+            src='https://media.istockphoto.com/id/635966596/vector/angry-bear-against-and-russian-flag.jpg?s=612x612&w=0&k=20&c=x2-XWzk96Zylu59BYcuFa2K98Jlx7HhFmog1ofGTbBc='
+            alt='phillipkononenko'
+          />
+
+          <p className='description'>
+           I'm a Russian-American who lives in Colorado. I love God. I love sports and being active.
+           I also like learning how to code and make things which I think is pretty cool.
+           I have two siberian huskies who are the best. I used to play soccer, boxing, martial arts, karate, and basketball. I currently now just do weightlifting and workout to stay healthy.
+           I hope to further my academic and professional career and break into the Tech Industry soon after I get the necessary skills to succeed. I have a small background in CS working with Python only
+           breifly but I am fairly new and eager to learn. 
+          </p>
+
+          <a className='button' href='/'>
+            <p> Back to Home Page</p>
+          </a>
+        </main>
+
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            padding: 0 0.5rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            max-width: 60rem;
+            margin: auto;
+          }
+
+          main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 4rem;
+          }
+
+          .underline {
+            /* border-bottom: solid black; */
+            margin-bottom: 1.5rem;
+          }
+
+          .description {
+            line-height: 1.5;
+            font-size: 1.5rem;
+          }
+
+          .pic {
+            height: 450px;
+            width: 450px;
+          }
+
+          .button {
+            /* margin: .5rem; */
+            flex-basis: 45%;
+            padding: 0.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+
+          .button:hover,
+          .button:focus,
+          .button:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .button p {
+            font-size: 1rem;
+            font-weight: 600;
+          }
+
+          code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+        `}</style>
+
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
+    );
+}
